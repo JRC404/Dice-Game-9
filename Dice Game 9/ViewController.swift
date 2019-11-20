6a2f7030a5316ad014e6d0ebcda919c2170917bf
Jacob Reilly-Cooper <jacob.reilly-cooper@wearecodenation.com>
2019-11-20 16:53:38 +0000

    Comments added for the dice array. @IBOutlets added for the labels, views and buttons. Soon to be linked up to the storyboard.
    The functions have been added.

--- Dice Game 9/ViewController.swift
+++ Dice Game 9/ViewController.swift
@@ -15,10 +15,30 @@ class ViewController: UIViewController {
     var playerScore = 0
     var dice = [#imageLiteral(resourceName: "Kismet-Ace"), #imageLiteral(resourceName: "Kismet-Deuce"), #imageLiteral(resourceName: "Kismet-Trey"), #imageLiteral(resourceName: "Kismet-Four"), #imageLiteral(resourceName: "Kismet-Five"), #imageLiteral(resourceName: "Kismet-Six")]
 //               0,  1,  2,  3,  4,  5
+// The above numbers are the index value of the dice corresponding to its true value.
     
     //MARK: Outlets
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var rulesLabel: UILabel!
+    @IBOutlet weak var mainMessageLabel: UILabel!
+    @IBOutlet weak var diceImage: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
+    /*
+     
+        The game logic: Roll a 1, you lose. Score 21 or higher and you win.
+     
+     */
     
     //MARK: Functions
+    
+    @IBAction func rollButtonAction (_ sender: Any) {
+        
+    }
+    
+    func diceLogic() {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
