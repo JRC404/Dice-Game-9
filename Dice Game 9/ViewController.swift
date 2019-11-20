6dc54bb148ba7703b3c3ad1513b2f9b2d8d69dfd
Jacob Reilly-Cooper <jacob.reilly-cooper@wearecodenation.com>
2019-11-20 14:13:36 +0000

    Basic project set up. //MARK: set up for our variables, outlets and function. Assets for the dice included and linked to the variable: dice.

--- Dice Game 9/ViewController.swift
+++ Dice Game 9/ViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //MARK: Variables
+    var randomDiceIndex = 0
+    var playerScore = 0
+    var dice = [#imageLiteral(resourceName: "Kismet-Ace"), #imageLiteral(resourceName: "Kismet-Deuce"), #imageLiteral(resourceName: "Kismet-Trey"), #imageLiteral(resourceName: "Kismet-Four"), #imageLiteral(resourceName: "Kismet-Five"), #imageLiteral(resourceName: "Kismet-Six")]
+//               0,  1,  2,  3,  4,  5
+    
+    //MARK: Outlets
+    
+    //MARK: Functions
 
     override func viewDidLoad() {
         super.viewDidLoad()
